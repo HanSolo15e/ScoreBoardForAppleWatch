2784be18302dc68e5ac96ab3c119a536e03ccf60
Evan Perry <evan.a.perry@icloud.com>
2023-12-30 15:08:19 -0800

    Initial Commit

---
+++ Score Board Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Score Board Watch App
+//
+//  Created by Evan Perry on 12/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Score Board Watch App/Score_BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Score_BoardApp.swift
+//  Score Board Watch App
+//
+//  Created by Evan Perry on 12/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Score_Board_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
