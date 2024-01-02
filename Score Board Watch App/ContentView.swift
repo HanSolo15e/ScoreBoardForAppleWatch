86d2d4d780a60a46140ec483422251119bf95980
Evan Perry <evan.a.perry@icloud.com>
2024-01-02 12:35:10 -0800

    buttons would not respond when not clicked in the center, now fixed

--- Score Board Watch App/ContentView.swift
+++ Score Board Watch App/ContentView.swift
@@ -13,70 +13,73 @@ struct ContentView: View {
     @State private var RanWin = 10;
     let RanWinTable = [5,10,15,20]
     var body: some View {
-    HStack(spacing: 10
-) {
-        VStack(spacing: 10) {
-            if Win == true {
-                Text("You Win!")
-                    .padding()
-                    .background(.green)
-                    .clipShape(.capsule)
+        HStack(spacing: 20) {
+            VStack(spacing: 10) {
+                if Win == true {
+                    Text("You Win!")
+                        .padding()
+                        .background(.green)
+                        .clipShape(.capsule)
                     
-            }else {
-                Text("\(RanWin - NumScore)" + " Points To Win")
-                    .padding()
-                    .multilineTextAlignment(.center)
-                    .clipShape(.capsule)
-            }
+                }else {
+                    Text("\(RanWin - NumScore)" + " Points To Win")
+                        .padding()
+                        .multilineTextAlignment(.center)
+                        .clipShape(.capsule)
+                }
                 
-            Button(){
-                RanWin = RanWinTable.randomElement() ?? 10
-                NumScore = 0
-                Win = false
-            }label: {
-                Text("Random Goal")
-            
-            }
-            .padding()
-        }
-        VStack {
-            Button(){
-                if NumScore != RanWin {
+                Button("Random Goal"){
+                    RanWin = RanWinTable.randomElement() ?? 10
+                    NumScore = 0
                     Win = false
-                    NumScore += 1
                 }
-                if NumScore == RanWin {
-                    Win = true
-                    return
-                }
-                
-            }label: {
-                Image(systemName: "arrowshape.up.fill")
-            }.padding()
-                .frame(width: 80, height: 50)
                 .buttonStyle(.plain)
-                .background(.green)
-                .clipShape(.capsule)
-            
-            
-            Text("\(NumScore)")
-            Button(){
-                if NumScore <= 0 {
-                    return
-                }
-                Win = false
-                NumScore -= 1
-            }label: {
-                Image(systemName: "arrowshape.down.fill")
+                .padding(10)
+                .background(.fill)
+                .clipShape(.buttonBorder)
             }
-            .frame(width: 80, height: 50)
-            .buttonStyle(.plain)
-            .background(.red)
-            .clipShape(.capsule)
+            VStack {
+                Button(){
+                    if NumScore != RanWin {
+                        Win = false
+                        NumScore += 1
+                    }
+                    if NumScore == RanWin {
+                        Win = true
+                        return
+                    }
+                    
+                }label: {
+                    Image(systemName: "arrowshape.up.fill")
+                        .padding(.vertical, 15)
+                        .padding(.horizontal, 25)
+                        .background(.green)
+                        .clipShape(.capsule)
+                }
+                .buttonStyle(.plain)
                 
+                
+                
+                Text("\(NumScore)")
+                Button(){
+                    if NumScore <= 0 {
+                        return
+                    }
+                    Win = false
+                    NumScore -= 1
+                }label: {
+                    Image(systemName: "arrowshape.down.fill")
+                        .padding(.vertical, 15)
+                        .padding(.horizontal, 25)
+                        .background(.red)
+                        .clipShape(.capsule)
+                    
+                }.buttonStyle(.plain)
+                
+                
+            }
+            
         }
-       
-    }
     }
 }
 
