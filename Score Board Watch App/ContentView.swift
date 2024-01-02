11a330b953ab5ca315a9af4d718e6f232e2fa455
Evan Perry <evan.a.perry@icloud.com>
2024-01-01 21:36:12 -0800

    Fixed the bug wera you have to Click 11 instead of 10 for it to say win, and add button for random winnig score dose not function yet

--- Score Board Watch App/ContentView.swift
+++ Score Board Watch App/ContentView.swift
@@ -10,9 +10,11 @@ import SwiftUI
 struct ContentView: View {
     @State private var NumScore = 0;
     @State private var Win = false;
+    @State private var RanWin = 0;
     var body: some View {
-    HStack {
-        VStack {
+    HStack(spacing: 10
+) {
+        VStack(spacing: 10) {
             if Win == true {
                 Text("You Win!")
                     .padding()
@@ -20,25 +22,35 @@ struct ContentView: View {
                     .clipShape(.capsule)
                     
             }else {
-                Text("\(10 - NumScore)" + " Points")
+                Text("\(10 - NumScore)" + " Points To Win")
                     .padding()
-                    .background(.blue)
+                    .multilineTextAlignment(.center)
                     .clipShape(.capsule)
             }
+                
+            Button(){
+                
+            }label: {
+                Text("Random Goal")
+                    
+            }
+            .padding()
         }
         VStack {
             Button(){
-                if NumScore >= 10 {
+                if NumScore != 10 {
+                    Win = false
+                    NumScore += 1
+                }
+                if NumScore == 10 {
                     Win = true
                     return
-                } else {
-                    Win = false
                 }
-                NumScore += 1
+                
             }label: {
                 Image(systemName: "arrowshape.up.fill")
             }.padding()
-                .frame(width: 100, height: 50)
+                .frame(width: 80, height: 50)
                 .buttonStyle(.plain)
                 .background(.green)
                 .clipShape(.capsule)
@@ -54,7 +66,7 @@ struct ContentView: View {
             }label: {
                 Image(systemName: "arrowshape.down.fill")
             }
-            .frame(width: 100, height: 50)
+            .frame(width: 80, height: 50)
             .buttonStyle(.plain)
             .background(.red)
             .clipShape(.capsule)
